9a98eb5961f642daf506af0e2dab0cc4c7fcc19f
Preston Wallace <wallace.preston@gmail.com>
2018-11-12 16:49:14 -0800

    final product

--- seed.js
+++ seed.js
@@ -1,10 +1,12 @@
 const {db, Gardener, Vegetable, Plot} = require('./models.js');
 
-db.sync({force: true}).then(
+db.sync({force: false}).then(
     () => {
         console.log("db synced!");
     }
-).then( Vegetable.create({name: 'asparagus', color: 'green', planted_on: new Date()}) )
+).then( () => { return Vegetable.create({name: 'asparagus', color: 'green', planted_on: new Date()}) } ) 
+.then((vegetable) => { return Gardener.create({name: 'Preston', age: 32, favoriteVegetable: vegetable.id})})
+.then((gardener) => { return Plot.create({size: 5, shaded: true, gardenerId: gardener.id})})
 .catch( (error) => {
     console.log("failed to sync");
     console.log(error);
